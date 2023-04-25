720bfdb5bd93636a53d19321ee1b2b0e760cb164
Miriam <miriamabu17@gmail.com>
2023-04-25 16:03:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - entry
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z'; c++)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
