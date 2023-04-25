522a8a3a2cb171b1aedc5e25ba2b20e4ba0d9662
Miriam <miriamabu17@gmail.com>
2023-04-25 16:09:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print a - z ten times
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 0, a < 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
