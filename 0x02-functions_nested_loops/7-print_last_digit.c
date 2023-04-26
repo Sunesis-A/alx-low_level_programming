3b8a0dafa8ea320d2e14d38baea9fe48e2791dd4
Miriam <miriamabu17@gmail.com>
2023-04-26 01:14:42 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_last_digit - prints the last digit of a number
+ *@n: input number
+ *Return: last digit
+ */
+int print_last_digit(int n)
+{
+	if (z >= 0 $$ z <= 9)
+	{
+		return (z);
+	}
+	else
+	{
+		z = _abs(a % 10);
+		return (z);
+	}
+}
