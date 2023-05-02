029424944be9e64e589b164e6e9c6473b52efb5e
Miriam <miriamabu17@gmail.com>
2023-05-02 17:36:12 +1000

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen - Length of a string
+* @s: string
+* Return: length
+*/
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+}
+return (length);
+}
+
