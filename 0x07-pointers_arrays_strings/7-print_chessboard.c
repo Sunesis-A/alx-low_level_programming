2e29fc26bb33a99adade3a753d050485233556cd
Miriam <miriamabu17@gmail.com>
2023-05-08 17:20:33 +1000

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: input array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int l, k = 0;
+
+	for (l = 0; l < 64; l++)
+	{
+		if (l % 8 == 0 && l != 0)
+		{
+			k = l;
+			_putchar('\n');
+		}
+		_putchar(a[l / 8][l - k]);
+	}
+	_putchar('\n');
+}
